ebc14595fdbda2b8f7e6200b806872e44318c462
Yauheni Hulinski <riggedman22@gmail.com>
2020-03-22 23:58:39 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let regex =/(\(\))|(\{\})|(\[\])|(\|\|)/;
+  let regex1 = /(12)|(34)|(56)|(77)|(88)/;
+  console.log(str)
+  
+  if(str.includes("1")){
+  
+  do{
+  str = str.replace(/(12)/g,"").replace(/(34)/g,"").replace(/(56)/g,"").replace(/(77)/g,"").replace(/(88)/g,"")
+  }
+  
+  while(regex1.test(str))
+  console.log(str)
+  }else{
+  do{
+  str = str.replace(/(\(\))/g,"").replace(/(\{\})/g,"").replace(/(\[\])/g,"").replace(/(\|\|)/g,"")
+  }while(regex.test(str))
+  }
+  console.log(str)
+  if (str === '') {
+  return true
+  }else{
+  return false
+  }
+  
+  
+  }
+  
